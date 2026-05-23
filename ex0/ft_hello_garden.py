524b2a547e5b270891bcf5cfb59286e5bc4c5713
Jayci Gan <220996564+jaycigan05@users.noreply.github.com>
2026-05-23 21:41:45 +0800

    Add ex0: ft_hello_garden

---
+++ ex0/ft_hello_garden.py
@@ -0,0 +1,10 @@
+# --------------------------------------------------------------------------- #
+#  File    : ft_hello_garden.py
+#  Project : Growing Code - Python 00
+#  Author  : jayci <jagan@student.42kl.edu.my>
+#  Created : 2026/05/23 21:22
+#  Updated : 2026/05/23 21:22
+# --------------------------------------------------------------------------- #
+
+def ft_hello_garden():
+    print("Hello, Garden Community!")
