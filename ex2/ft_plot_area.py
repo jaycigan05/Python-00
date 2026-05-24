5f98f22cf6a23ac0e791ea1adc79870171d427f7
Jayci Gan <220996564+jaycigan05@users.noreply.github.com>
2026-05-24 08:50:36 +0800

    ex2/ft_plot_area

---
+++ ex2/ft_plot_area.py
@@ -0,0 +1,13 @@
+# --------------------------------------------------------------------------- #
+#  File    : ft_plot_area.py
+#  Project : Python-00
+#  Author  : jagan <jagan@student.42kl.edu.my>
+#  Created : 2026/05/24 08:41
+#  Updated : 2026/05/24 08:41
+# --------------------------------------------------------------------------- #
+
+
+def ft_plot_area():
+    len = int(input("Enter length: "))
+    wid = int(input("Enter width: "))
+    print("Plot area:", (len * wid))
