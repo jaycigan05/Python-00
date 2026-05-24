554c0a7c5671db6adfc4b7e77f3a76bc4ee19ded
Jayci Gan <220996564+jaycigan05@users.noreply.github.com>
2026-05-24 08:26:13 +0800

    add: ex1/ft_garden_name

---
+++ ex1/ft_garden_name.py
@@ -0,0 +1,12 @@
+# --------------------------------------------------------------------------- #
+#  File    : ft_garden_name.py
+#  Project : Python-00
+#  Author  : jagan <jagan@student.42kl.edu.my>
+#  Created : 2026/05/23 21:54
+#  Updated : 2026/05/23 21:54
+# --------------------------------------------------------------------------- #
+
+def ft_garden_name():
+    garden = input("Enter tool name: ")
+    print("Garden:", garden)
+    print("Status: Growing well!")
