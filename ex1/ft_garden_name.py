9ea17f61d6226e49f2018369933a41fabf74a762
Jayci Gan <220996564+jaycigan05@users.noreply.github.com>
2026-05-24 08:39:15 +0800

    fix: flake8

--- ex1/ft_garden_name.py
+++ ex1/ft_garden_name.py
@@ -7,6 +7,6 @@
 # --------------------------------------------------------------------------- #
 
 def ft_garden_name():
-    garden = input("Enter tool name: ")
+    garden = input("Enter garden name: ")
     print("Garden:", garden)
     print("Status: Growing well!")
