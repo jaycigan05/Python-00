ff586ec7308d24582eec11db096afe166b5574bf
Jayci Gan <220996564+jaycigan05@users.noreply.github.com>
2026-05-24 09:02:58 +0800

    ex3/ft_harvest_total

---
+++ ex3/ft_harvest_total.py
@@ -0,0 +1,13 @@
+# --------------------------------------------------------------------------- #
+#  File    : ft_harvest_total.py
+#  Project : Python-00
+#  Author  : jagan <jagan@student.42kl.edu.my>
+#  Created : 2026/05/24 08:53
+# --------------------------------------------------------------------------- #
+
+
+def ft_harvest_total():
+    d1 = int(input("Day 1 harvest: "))
+    d2 = int(input("Day 2 harvest: "))
+    d3 = int(input("Day 3 harvest: "))
+    print("Total harvest:", (d1 + d2 + d3))
